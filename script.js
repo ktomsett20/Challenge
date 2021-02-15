1703a429f750c6cc7d9aade8f837688f92a580e1
pvahanian <51976194+pvahanian@users.noreply.github.com>
2021-02-14 21:26:16 -0600

    for buddy

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 // Default date selector (todays date)
 todaysDate();
 let language = document.querySelector("html").lang
-console.log(language)
 
 // ***
 // Function Goal: If client has a witness appends questions to div
 // Params: Expects event data to pull target to modify
 // ***
-
 function witnessChecker(event) {
   let witnessDiv = document.getElementById("witnessSwitch");
-  let questions =`<label for="">What is their first name?</label>
-  <input type="text" name="witnessFirstName" class="form-control" required />
-  <label for="">What is their last name?</label>
-  <input type="text" name="witnessLastname" class="form-control" required />
-  <label for="">What is their phone number?</label>
-  <input type="text" name="witnessPhone" class="form-control" required />`
-
+  let questions
   if(language ==="es"){
-    questions=``
+    questions=`<label for="">¿Cual es su primer nombre?</label>
+    <input type="text" name="witnessFirstName" class="form-control" value="" required />
+    <label for="">¿Cual es su apellido?</label>
+    <input type="text" name="witnessLastname" class="form-control" required />
+    <label for="">¿Cual es su numero de telefono?</label>
+    <input type="text"name="witnessPhone" class="form-control" required />`
+  } else{
+    questions =`<label for="">What is their first name?</label>
+    <input type="text" name="witnessFirstName" value="" class="form-control" required />
+    <label for="">What is their last name?</label>
+    <input type="text" name="witnessLastName" class="form-control" required />
+    <label for="">What is their phone number?</label>
+    <input type="text" name="witnessPhone" class="form-control" required />`
   }
-
   if (event.target.value==="Yes" && witnessDiv.style.display!=="flex") {
     witnessDiv.style.display = "flex";
     witnessDiv.innerHTML +=questions
   } else if(event.target.value==="No") {
     witnessDiv.style.display = "none";
-    witnessDiv.innerHTML=""
+    witnessDiv.innerHTML="";
   }
 }
 
@@ -34,30 +37,55 @@ function witnessChecker(event) {
 // Function Goal: If client has an attorney appends questions to div
 // Params: Expects event data to pull target to modify
 // ***
+
 function attorneyChecker(event) {
-  let lawyerDiv = document.getElementById("attorney-switch");
-  let questions = `<label for="">What is your attorney's name?</label>
-  <input type="text" name="attorneyName" class="form-control" value=" "  required />
-  <label for="">What is the name of the company they work for?</label>
-  <input type="text" name="attorneyCompany" class="form-control" value=" "  required />
-  <label for="">What is the companies website address?</label>
-  <input type="text" name="attorneySite" class="form-control" value=" "  required />`
+  let lawyerDiv = document.getElementById("attorneySwitch");
+  let questions
+  if(language ==="es"){
+    questions=`<label for="">¿Como se llama su abogada/abogado?</label>
+    <input type="text" name="attorneyName" class="form-control" required />
+    <label for="">¿Cuál es el nombre de la compania donde trabaja?</label>
+    <input type="text" name="attorneyCompany" class="form-control" required />
+    <label for="">¿Cuál es la dirección del sitio web de la compañia?</label>
+    <input type="text" name="attorneySite" class="form-control" required />
+  </div>`
+  }
+  else{
+    questions= `<label for="">What is your attorney's name?</label>
+    <input type="text" name="attorneyName" class="form-control" required />
+    <label for="">What is the name of the company they work for?</label>
+    <input type="text" name="attorneyCompany" class="form-control" required />
+    <label for="">What is the companies website address?</label>
+    <input type="text" name="attorneySite" class="form-control" required />`
+  }
 
 
   if (event.target.value==="Yes" && attorneySwitch.style.display!=="flex") {
     lawyerDiv.style.display = "flex";
-  } else {
+    lawyerDiv.innerHTML=(questions);
+  } else if(event.target.value==="No") {
     lawyerDiv.style.display = "none";
+    lawyerDiv.innerHTML="";
   }
 }
 
 function submitHandler(event) {
+  const testdata= document.getElementsByName("witnessFirstName")
+  console.log(testdata)
   event.preventDefault()
   const formData = new FormData(event.target);
   let formObjectData = {};
   formData.forEach((value, key) => formObjectData[key] = value);
   let jsonData = JSON.stringify(formObjectData);
-  console.log(jsonData)
+  // displayJson(jsonData)
+}
+
+function displayJson(data){
+  const showData = document.getElementById("replaceForm")
+  showData.innerHTML=""
+  showData.innerHTML=(`<h2>Json Data</h2>`)
+  showData.style.alignItems = "flex-start"
+  showData.innerHTML += data
 }
 
 function todaysDate() {
@@ -72,6 +100,3 @@ function todaysDate() {
   let today = year + "-" + month + "-" + day;
   document.getElementById("theDate").value = today;
   }
-
-
-
