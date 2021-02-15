f1d3572f980f9ceafcc01e34ffc61e067b854671
pvahanian <51976194+pvahanian@users.noreply.github.com>
2021-02-14 22:41:10 -0600

    Complete

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let language = document.querySelector("html").lang
 // Function Goal: If client has a witness appends questions to div
 // Params: Expects event data to pull target to modify
 // ***
+
 function witnessChecker(event) {
   let witnessDiv = document.getElementById("witnessSwitch");
   let questions
@@ -59,7 +60,6 @@ function attorneyChecker(event) {
     <input type="text" name="attorneySite" class="form-control" required />`
   }
 
-
   if (event.target.value==="Yes" && attorneySwitch.style.display!=="flex") {
     lawyerDiv.style.display = "flex";
     lawyerDiv.innerHTML=(questions);
@@ -69,24 +69,40 @@ function attorneyChecker(event) {
   }
 }
 
+// ***
+// Function Goal: Submit data to displayJson function while converting the formData to JSON
+// Params: Expects Event data for the whole form data
+// ***
+
 function submitHandler(event) {
-  const testdata= document.getElementsByName("witnessFirstName")
-  console.log(testdata)
   event.preventDefault()
   const formData = new FormData(event.target);
   let formObjectData = {};
   formData.forEach((value, key) => formObjectData[key] = value);
   let jsonData = JSON.stringify(formObjectData);
-  // displayJson(jsonData)
+  displayJson(jsonData)
 }
+
+// ***
+// Function Goal: Take JSON data and display it while removing all the forms
+// Params: Expects data as JSON data
+// ***
 
 function displayJson(data){
   const showData = document.getElementById("replaceForm")
   showData.innerHTML=""
   showData.innerHTML=(`<h2>Json Data</h2>`)
   showData.style.alignItems = "flex-start"
-  showData.innerHTML += data
+  const pTag = document.createElement("p")
+  pTag.textContent=data
+  showData.appendChild(pTag)
+  showData.style.overflow="auto"
 }
+
+// ***
+// Function Goal: Get todays date and set it up as the default
+// Params: None
+// ***
 
 function todaysDate() {
   let date = new Date();
